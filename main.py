8411ac4d61b7cde93e11ce8cd27bc5461f3e2fd8
Danilo Abreu <daabreu@sga.pucminas.br>
2022-04-01 21:19:36 -0300

    adicionando merda ao projeto, não deu certo

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import os
+from datetime import datetime
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
+
+
+basedir = os.path.abspath(os.path.dirname(__file__)
 app = Flask(__name__)
+
+app.config[‘SQLALCHEMY_DATABASE_URI’] =\‘sqlite:///’ + os.path.join(basedir, ‘todo.sqlite’)
+app.config[‘SQLALCHEMY_TRACK_MODIFICATIONS’] =  False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Task(db.Model):
+id = db.Column(db.Integer, primary_key=True)
+description = db.Column(db.String(200), nullalble=False)
+date_created = db.Column(db.DateTime, default=datetime.utcnow)
+def __repr__(self):
+return f“Task: #{self.id}, description: {self.description}”
 
 Bootstrap(app)
 
